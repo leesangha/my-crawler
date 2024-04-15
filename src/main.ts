858a3a5feab575482daf4f9eca236ac371b654e3
leesangha <sanghalee1@naver.com>
2024-04-15 17:07:33 +0900

    feat: Analytics crawling

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@
 import { PlaywrightCrawler } from "crawlee";
 import { router } from "./routes.js";
 import { LABELS } from "./constants/label.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 // const startUrls = ["https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"];
 const startUrls = [
@@ -13,10 +15,11 @@ const startUrls = [
   //   url: "https://edition.cnn.com/markets/fear-and-greed",
   //   label: LABELS.FEARANDGREED,
   // },
-  {
-    url: "https://m.stock.naver.com/worldstock/index/.VIX/total",
-    label: LABELS.VIX,
-  },
+  // {
+  //   url: "https://m.stock.naver.com/worldstock/index/.VIX/total",
+  //   label: LABELS.VIX,
+  // },
+  { url: "https://github.com" },
 ];
 
 const crawler = new PlaywrightCrawler({
--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,88 @@ import { LABELS } from "./constants/label.js";
 
 export const router = createPlaywrightRouter();
 
-router.addDefaultHandler(async ({}) => {});
+router.addDefaultHandler(async ({ page }) => {
+  const email: string = process.env.TEST_EMAIL as string;
+  const password: string = process.env.TEST_PASSWORD as string;
+
+  await page.goto("https://github.com/");
+  await page.getByRole("link", { name: "Sign in" }).click();
+  await page.getByLabel("Username or email address").click();
+
+  await page.getByLabel("Username or email address").fill(email);
+  await page.getByLabel("Password").click();
+  await page.getByLabel("Password").fill(password);
+  await page.getByRole("button", { name: "Sign in", exact: true }).click();
+
+  await page.goto("https://vercel.com/login?next=%2Finfludeo");
+  await page.getByTestId("login/github-button").click();
+  await page.waitForURL("https://vercel.com/infludeo");
+  await page.goto(
+    "https://vercel.com/infludeo/react-global-web-prod/analytics"
+  );
+  await page.waitForURL(
+    "https://vercel.com/infludeo/react-global-web-prod/analytics"
+  );
+
+  // filter 24시간 단위로 분리
+  await page.selectOption(
+    "body > div > main > div > section > div > div:nth-child(1) > header > div > div > div > div.stack.self-stretch > div > div.stack > label:nth-child(2)",
+    "24h"
+  );
+  await page.waitForLoadState();
+
+  // Operation Systems
+  await page
+    .locator(
+      "body > div > main > div > section > div > div.relative.flex-1 > div > div > div > div > div.stack > div:nth-child(2) > div:nth-child(2) > button"
+    )
+    .click();
+
+  // 추출
+  const _locator = page.locator(
+    ".tailwind > div > div.flex.flex-col.my-2 > button"
+  );
+  const dt = await _locator.all();
+  const count = await _locator.count();
+
+  const OS_Headers = ["Operating Systems", "VISITORS", "PAGE VIEWS"];
+  let inner = {};
+  for (let i = 0; i < count; i++) {
+    const innerData = await dt[i].locator("p").allInnerTexts();
+    inner = { ...inner, [i]: innerData };
+  }
+
+  // 저장
+  const store = await KeyValueStore.open("OPERATING_SYSTEM");
+  await store.setValue("OPERATING_SYSTEM", inner);
+  // 종료
+  await page.getByText("close").click();
+
+  // Browsers
+  await page
+    .locator(
+      "body > div > main > div > section > div > div.relative.flex-1 > div > div > div > div > div.stack > div:nth-child(2) > div:nth-child(3) > button"
+    )
+    .click();
+  await page.waitForLoadState();
+  const __locator = page.locator(
+    "div.geist-overlay.tailwind > div > div > div > div:nth-child(1) > div.my-2 > div > div"
+  );
+  const browser_locator = __locator.locator("> div");
+  const browser_data = await browser_locator.all();
+  const browser_count = await browser_locator.count();
+
+  const Browsers_Headers = ["Browsers", "VISITORS", "PAGE VIEWS"];
+  let browser_inner = {};
+  for (let i = 0; i < browser_count; i++) {
+    console.log("browser crawl[", i, "]");
+    const innerData = await browser_data[i].locator("p").allInnerTexts();
+    browser_inner = { ...browser_inner, [i]: innerData };
+  }
+  const browser_store = await KeyValueStore.open("Browsers");
+  await browser_store.setValue("Browsers", browser_inner);
+  await page.getByText("close").click();
+});
 
 router.addHandler(LABELS.TICKERS, async ({ page, log }) => {
   const title = await page.title();
