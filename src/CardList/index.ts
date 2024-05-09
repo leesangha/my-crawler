5577e5ffcf5d8efd6c128f83b1c3e86fc1126463
leesangha <sanghalee1@naver.com>
2024-05-09 12:43:57 +0900

    add: album crawl

--- src/CardList/index.ts
+++ src/CardList/index.ts
@@ -9,14 +9,17 @@ export async function CardList({ page }: { page: Page }) {
     "https://api.pocamarket.com/card/gb/v1/group"
   );
 
-  const groupIdList = data.groups.filter((group) => group.id === 21);
-  // const groupIdList = [21];
-  console.log("groupList", groupIdList);
+  const finalArray = [];
+  const groupIdList = data.groups;
+  // const groupIdList = [];
 
   // 그룹에 맞는 전체 앨범 리스트 가져오기
-  for (let i = 0; i < groupIdList.length; i++) {
+  for (
+    let i = Math.floor(groupIdList.length / 2);
+    i < groupIdList.length;
+    i++
+  ) {
     const group = groupIdList[i];
-    await page.goto("https://melon.com");
 
     await page.goto("https://www.melon.com/index.htm");
     await page.getByRole("textbox", { name: "검색 입력 편집창" }).click();
@@ -32,16 +35,52 @@ export async function CardList({ page }: { page: Page }) {
     const _locator = page.locator("dl");
     const albumArray = await _locator.all();
     const albumCount = await _locator.count();
-    const albumTitle = _locator.locator("dt");
 
-    console.log(albumCount);
+    console.log("--> crawl start", group.name);
+    // 앨범 타입
+    // const albumType = await _locator.locator(".vdo_name").innerText();
+    // console.log(albumType);
+    const kvDataArray = [];
+
     for (let j = 0; j < albumCount; j++) {
-      const album = albumArray[j].locator("> dd");
-      const data = await album.allInnerTexts();
-      console.log(data);
+      const target = albumArray[j];
+      const _album_info = target.locator("dt");
+      const album = target.locator("dd.btn_play");
+
+      // 앨범 타입
+      const albumType = await _album_info.locator(".vdo_name").innerText();
+
+      // 앨범 이름
+      const albumTitle = await _album_info.locator("a").innerText();
+
+      // 아티스트 이름
+      const artistName = await target
+        .locator(".checkEllipsisSearchAlbum")
+        .innerText();
+
+      // 곡 명
+      const songName = await album.locator(".songname12").innerText();
+
+      // 발매일
+      const date = await target.locator("span.cnt_view").innerText();
+
+      // 곡 수
+      const nums = await target.locator("span.tot_song").innerText();
+
+      const data = {
+        type: albumType,
+        title: albumTitle,
+        name: artistName,
+        title_song: songName,
+        date,
+        song_count: nums,
+      };
+
+      kvDataArray.push(data);
     }
+    finalArray.push({ group, albums: kvDataArray });
   }
 
   const store = await KeyValueStore.open(LABELS.CARDLIST);
-  await store.setValue(LABELS.CARDLIST, data);
+  await store.setValue(LABELS.CARDLIST, finalArray);
 }
